4571a99b8cea6e2c7bd30e4e7b71137ac05845c4
Sadhna Sharma <75626442+sadhna88059@users.noreply.github.com>
2023-02-28 17:23:44 +0200

    prolog2.pl

---
+++ prolog2.pl
@@ -0,0 +1,11 @@
+# Write a Prolog program to implement max(X, Y, M) so that M is the maximum of two numbers X and Y.
+
+max(X, Y, M):- 
+    X>Y, M is X,
+    write('Maximum is '),
+    write(M).
+
+max(X, Y, M):- 
+    Y>=X, M is Y,
+    write('Maximum is '),
+    write(M).
